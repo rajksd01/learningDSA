d8434e43428d932c2b5d6e2d432f47ab305205b3
Raj Kasaudhan <71208599+rajksd01@users.noreply.github.com>
2023-11-02 23:37:59 +0530

    Create LinkedList.java

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,26 @@
+// Implementation of LinkedList 
+import java.util.*; 
+
+public class linklist { 
+  
+	public static void main(String args[]) 
+	{  
+		LinkedList<String> ll = new LinkedList<String>(); 
+
+		// Adding elements to the linked list 
+		ll.add("A"); 
+		ll.add("B"); 
+		ll.addLast("C"); 
+		ll.addFirst("D"); 
+		ll.add(2, "E"); 
+
+		System.out.println(ll); 
+
+		ll.remove("B"); 
+		ll.remove(3); 
+		ll.removeFirst(); 
+		ll.removeLast(); 
+
+		System.out.println(ll); 
+	} 
+}
